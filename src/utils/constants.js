19d54e51faeba59347cb0c29a9a3235d016e6260
Syed Ridwanuddin <syedridwan0301@gmail.com>
2025-03-19 11:10:00 +0530

    cors allow

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const CON_URL= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=14.4673154&lng=78.8242089&restaurantId=";
+export const MENU_API="https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=14.4673154&lng=78.8242089&restaurantId=";
